3c26df41e9cfc21d10e7d1b6339f42d66945446f
Bobby Towers <porkostomus@mail.com>
2020-02-14 02:15:58 -0800

    bring in machinery for live composition

--- bb/bb-sox.clj
+++ bb/bb-sox.clj
@@ -20,6 +20,15 @@
             (str (midi->freq (Integer/parseInt pitch)))
             "trim" "0" len))
 
+(defn init-wav []
+  (shell/sh "sox" "-n" "-c" "1" "samples/temp0.wav" "trim" "0.0" "0.0"))
+
+(defn append-note [wave pitch val]
+  (shell/sh "sox" "samples/temp0.wav" 
+            (str "samples/note-" pitch "-" wave "-" val ".wav")
+            "samples/temp1.wav")
+  (shell/sh "mv" "samples/temp1.wav" "samples/temp0.wav"))
+
 (defn play [file]
   (shell/sh "play" file))
 
@@ -31,12 +40,23 @@
   (let [files (str/split-lines (:out (shell/sh "ls")))]
     (when-not (some #{(str "samples/note-" pitch "-" wave "-whole.wav")} files)
       (doseq [[val len] (notes (Integer/parseInt tempo))]
-        (create-note wave pitch (name val) (str (float len)))))))
+        (create-note wave (str pitch) (name val) (str (float len)))))))
+
+(defn build-track [file wave notes]
+  (init-wav)
+  (println (str "Building track - " file))
+  (doseq [[pitch val] notes]
+    (create-notes wave pitch)
+    (append-note wave pitch val))
+  (shell/sh "mv" "samples/temp0.wav" (str file ".wav")))
 
 (comment
 
-  (create-notes "triangle" "42")
+  (create-notes "triangle" 42)
   
-  (play "samples/note-42-triangle-whole.wav")
+  (build-track "bass-1" "triangle"
+               [[40 "eighth"] [38 "eighth"] [36 "quarter"] [35 "whole"]])
+  
+  (play "bass-1.wav")
 
   )
